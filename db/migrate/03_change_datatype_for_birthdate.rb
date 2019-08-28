9dbcea13f47fef8db547ae5387a25a014e8a9722
ZumbaIdol <david.a.foote@gmail.com>
2019-08-27 19:10:26 -0500

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
-ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
-def change
-  change_column :students, :type, :datetime 
+  def change
+    change_column :students, :birthdate, :datetime
+    end
   end
-end
